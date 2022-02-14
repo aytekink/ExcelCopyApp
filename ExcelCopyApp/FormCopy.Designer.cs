4867e8728353cd065dbee6d9c6880fe7a5896371
kadir.aytekin <kadir.aytekin@live.com>
2022-02-14 12:09:33 +0300

    Add project files.

---
+++ ExcelCopyApp/FormCopy.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace WinFolderApp
+{
+    partial class FormCopy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtKaynak = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtHedef = new System.Windows.Forms.TextBox();
+            this.btnKaynak = new System.Windows.Forms.Button();
+            this.btnHedef = new System.Windows.Forms.Button();
+            this.btnExcel = new System.Windows.Forms.Button();
+            this.txtExcel = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.folderBrowserDialog_Hedef = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserDialog_Kaynak = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // txtKaynak
+            // 
+            this.txtKaynak.Location = new System.Drawing.Point(145, 26);
+            this.txtKaynak.Name = "txtKaynak";
+            this.txtKaynak.ReadOnly = true;
+            this.txtKaynak.Size = new System.Drawing.Size(441, 22);
+            this.txtKaynak.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kaynak Klasör";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hedef Klasör";
+            // 
+            // txtHedef
+            // 
+            this.txtHedef.Location = new System.Drawing.Point(145, 76);
+            this.txtHedef.Name = "txtHedef";
+            this.txtHedef.ReadOnly = true;
+            this.txtHedef.Size = new System.Drawing.Size(441, 22);
+            this.txtHedef.TabIndex = 3;
+            // 
+            // btnKaynak
+            // 
+            this.btnKaynak.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKaynak.Location = new System.Drawing.Point(592, 26);
+            this.btnKaynak.Name = "btnKaynak";
+            this.btnKaynak.Size = new System.Drawing.Size(75, 23);
+            this.btnKaynak.TabIndex = 4;
+            this.btnKaynak.Text = "Seç";
+            this.btnKaynak.UseVisualStyleBackColor = true;
+            this.btnKaynak.Click += new System.EventHandler(this.btnKaynak_Click);
+            // 
+            // btnHedef
+            // 
+            this.btnHedef.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHedef.Location = new System.Drawing.Point(592, 76);
+            this.btnHedef.Name = "btnHedef";
+            this.btnHedef.Size = new System.Drawing.Size(75, 23);
+            this.btnHedef.TabIndex = 5;
+            this.btnHedef.Text = "Seç";
+            this.btnHedef.UseVisualStyleBackColor = true;
+            this.btnHedef.Click += new System.EventHandler(this.btnHedef_Click);
+            // 
+            // btnExcel
+            // 
+            this.btnExcel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnExcel.Location = new System.Drawing.Point(592, 124);
+            this.btnExcel.Name = "btnExcel";
+            this.btnExcel.Size = new System.Drawing.Size(75, 23);
+            this.btnExcel.TabIndex = 8;
+            this.btnExcel.Text = "Seç";
+            this.btnExcel.UseVisualStyleBackColor = true;
+            this.btnExcel.Click += new System.EventHandler(this.btnExcel_Click);
+            // 
+            // txtExcel
+            // 
+            this.txtExcel.Location = new System.Drawing.Point(145, 125);
+            this.txtExcel.Name = "txtExcel";
+            this.txtExcel.ReadOnly = true;
+            this.txtExcel.Size = new System.Drawing.Size(441, 22);
+            this.txtExcel.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Excel Doyası";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(686, 26);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(575, 628);
+            this.listBox1.TabIndex = 9;
+            // 
+            // FormCopy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1273, 665);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnExcel);
+            this.Controls.Add(this.txtExcel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnHedef);
+            this.Controls.Add(this.btnKaynak);
+            this.Controls.Add(this.txtHedef);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtKaynak);
+            this.Name = "FormCopy";
+            this.Text = "FormCopy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKaynak;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtHedef;
+        private System.Windows.Forms.Button btnKaynak;
+        private System.Windows.Forms.Button btnHedef;
+        private System.Windows.Forms.Button btnExcel;
+        private System.Windows.Forms.TextBox txtExcel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog_Hedef;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog_Kaynak;
+    }
+}
---
+++ ExcelCopyApp/FormCopy.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFolderApp
+{
+    public partial class FormCopy : Form
+    {
+        public FormCopy()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKaynak_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog_Kaynak.SelectedPath = txtKaynak.Text;
+            DialogResult drResult = folderBrowserDialog_Kaynak.ShowDialog();
+            if (drResult == System.Windows.Forms.DialogResult.OK)
+                txtKaynak.Text = folderBrowserDialog_Kaynak.SelectedPath;
+        }
+
+        private void btnHedef_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog_Hedef.SelectedPath = txtHedef.Text;
+            DialogResult drResult = folderBrowserDialog_Hedef.ShowDialog();
+            if (drResult == System.Windows.Forms.DialogResult.OK)
+                txtHedef.Text = folderBrowserDialog_Hedef.SelectedPath;                
+        }
+
+        private void btnExcel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
